2e06d48b9c001a69b56289a36df9faf04873790a
mcb <mgrbo@web.de>
2020-04-12 18:23:19 +0200

    v0.5.8

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision 
 
-[assembly: AssemblyVersion("0.5.7")]
+[assembly: AssemblyVersion("0.5.8")]
 
 public static class VersionHelper
 {
