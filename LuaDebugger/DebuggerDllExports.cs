4034de7ea19cd44cd1eafb88837348e22b564b11
mcb <mgrbo@web.de>
2022-04-23 23:01:28 +0200

    mostly working net upgrade

--- LuaDebugger/DebuggerDllExports.cs
+++ LuaDebugger/DebuggerDllExports.cs
@@ -1,5 +1,4 @@
-﻿using RGiesecke.DllExport;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Diagnostics;
--- LuaDebugger/Helper/DbgThread.cs
+++ LuaDebugger/Helper/DbgThread.cs
@@ -9,29 +9,9 @@ namespace LuaDebugger
 {
     public static class DbgThread
     {
-        static Dictionary<string, Assembly> LoadedAssemblies = new Dictionary<string, Assembly>();
-
         [STAThread] //attribute doesnt work, needs to be done explicitly on the thread object
         public static void RunMessageLoop()
         {
-            string[] dllsToLoad = new string[] 
-            {
-                "LuaDebugger.Resources.ICSharpCode.TextEditor.dll"
-            };
-            Assembly assembly = Assembly.GetExecutingAssembly();
-            foreach (string dll in dllsToLoad)
-            {
-                using (Stream stm = assembly.GetManifestResourceStream(dll))
-                {
-                    byte[] ba = new byte[stm.Length];
-                    stm.Read(ba, 0, (int)stm.Length);
-                    Assembly a = Assembly.Load(ba);
-                    LoadedAssemblies.Add(a.FullName, a);
-                }
-            }
-
-            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
-
             if (!GlobalState.IsInVisualStudio) //better to show exceptions directly in the debugger
             {
                 AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
@@ -42,17 +22,6 @@ namespace LuaDebugger
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new DbgMain());
-        }
-
-        static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
-        {
-            Assembly asm;
-            if (!LoadedAssemblies.TryGetValue(args.Name, out asm))
-            {
-                MessageBox.Show("The following Assembly was not found:\n" + args.Name + "\nPlease verify that .NET Framework 3.5 is installed correctly.");
-                Environment.Exit(-1);
-            }
-            return asm;
         }
 
         static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
