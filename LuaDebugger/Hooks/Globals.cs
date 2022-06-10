77a9e59196003d07cc1cc6dffd1b12dd3f3e45e3
mcb <mgrbo@web.de>
2022-06-10 10:25:03 +0200

    better override of luasharps globals

---
+++ LuaDebugger/Hooks/Globals.cs
@@ -0,0 +1,16 @@
+﻿using LuaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuaSharp
+{
+    internal static class Globals
+    {
+        public const string Lua50Dll = LuaDebugger.GlobalState.LuaDll;// "lua50/lua50.dll";
+        public const string Lua51Dll = LuaDebugger.GlobalState.LuaDll;// "lua51/lua51.dll";
+    }
+}
